1e42d420d81afdc590c838e32071222f3802ae5c
Shaolin P <shiulan.poe@outlook.com>
2020-04-10 12:07:23 -0400

    Update by Shaolin

--- Routes_Filters_ModelBinding_Validation/Controllers/ModelBindingController.cs
+++ Routes_Filters_ModelBinding_Validation/Controllers/ModelBindingController.cs
@@ -37,5 +37,23 @@ namespace Routes_Filters_ModelBinding_Validation.Controllers
     public ViewResult NamesList(IList<string> names) => View(names ?? new List<string>());
 
     public ViewResult Address(IList<AddressSummary> addresses) => View(addresses ?? new List<AddressSummary>());
+
+
+    public ViewResult BindingCreate()
+    {
+        Course course = new Course();
+
+            return View(course);
+    }
+
+    [HttpPost]
+    public ViewResult BindingCreate(Course course)
+    {
+            return View(course);
+
+    }
+
+
+
   }
 }
